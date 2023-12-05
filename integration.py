683ccfe4462241986a63d71967d0bae081f7e4fa
imhollas <40440540+imhollas@users.noreply.github.com>
2023-12-05 09:27:47 -0600

    Removed redundant computations in algorithm

--- integration.py
+++ integration.py
@@ -15,10 +15,8 @@ def trapezoid_sum(a, b, n, func):
         Integrates func from a to b via trapezoid sum with n slices 
     """
     deltax = (b-a)/n
-    integral = 0
-    for i in range(0, n):
-        trapezoid_i = \
-                (func(a + i*deltax)+func(a + (i+1)*deltax))*deltax/2
-        integral += trapezoid_i
+    integral = (func(a)+func(b))*deltax/2
+    for i in range(1, n):
+        integral += deltax * func(a + i*deltax)
         
     return integral
