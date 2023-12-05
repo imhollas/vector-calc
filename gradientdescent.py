af08883e8e50628686c82c1f2613202deea27d92
imhollas <40440540+imhollas@users.noreply.github.com>
2023-12-05 14:58:14 -0600

    Finished. Added some code to test at the bottom.

--- gradientdescent.py
+++ gradientdescent.py
@@ -5,9 +5,39 @@ from gradient import grad
 from lineintegrals import dot
 import random
 
+def average(array):
+    """ 
+        array is a list containing floats. Returns the mean of the elements 
+        of the array 
+    """
+    avg = 0
+    for i in array:
+        avg += i
+
+    avg /= len(array)
+    return avg
+
+
+def stdev(array):
+    """
+        array is a list containing floats. Returns the standard deviation
+        of the elements of the array.
+    """
+    mean = average(array)
+    variance = 0
+    for xi in array:
+       variance += (mean - xi)**2
+
+    
+    variance /= len(array)
+    standard_deviation = variance ** 0.5
+
+    return standard_deviation
+
+
 def norm(u):
     """ Returns the Euclidean norm of the vector u """
-    length = dot(u, u)
+    length = (dot(u, u))**0.5
     return length
 
 
@@ -30,12 +60,13 @@ def descent(func, initial_pos, diffstep):
     current_grad = grad(func, diffstep, initial_pos)
 
     trial_num = 0
-    while norm(current_grad) > 10**(-5):
+    diffstepscaling = -1.0
+    while norm(current_grad) > 10**(-5) and trial_num < 10**7:
         scaled_current_grad = []
         # this loop makes scaled_current_grad equal to current_grad scaled 
         # by -0.1 times diffstep
         for i in range(len(initial_pos)):
-            scaled_current_grad.append(current_grad[i] * (-0.1) * diffstep)
+            scaled_current_grad.append(current_grad[i] * diffstepscaling * diffstep)
 
         # updating the position by stepping one tenth the value of the grad
         for n, x in enumerate(current_position):
@@ -45,18 +76,35 @@ def descent(func, initial_pos, diffstep):
         current_grad = grad(func, diffstep, current_position)
         trial_num += 1
 
-    print(trial_num)
-    print(current_grad)
-    return current_position
-
-initial_test = [7.3, 2.2]
-print(descent(function, initial_test, 0.01))
+    return [current_position, trial_num]
 
 
-for i in range(100):
-    # these generate random points in the unit disk
-    randx = (random.random() - 0.5)*2
-    randy = (random.random() - 0.5) * (1 - randx**2)**(0.5)
+# code for testing the function, specifically how many steps it takes
+num_steps_list = []
+for i in range(1000):
+    rand_x = 100 * random.random()
+    rand_y = 100 * random.random()
+    rand_pos = [rand_x, rand_y]
+
+    result = descent(function, rand_pos, 0.01)
+    num_steps = result[1]
+    position = result[0]
+    
+    dist_to_true = ((1-position[0])**2 + (1-position[1])**2)**0.5
+    
+    # the if statement verifies that we ended in the domain
+    # the trial is ignored if this did not happen
+    if (0 < position[0] and position[0] < 100) and \
+            (0 < position[1] and position[1] < 100) \
+            and num_steps != 10**7:
+        print("Converged on trial {0}".format(i))
+        print("Location was x = {0}, y = {1}".format(position[0], \
+                position[1]))
+
+        num_steps_list.append(num_steps)
 
 
-
+print(average(num_steps_list))
+print(stdev(num_steps_list))
+print(num_steps_list)
+print(len(num_steps_list)) # this is the number of times it converged
